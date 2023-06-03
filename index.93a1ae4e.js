5675e351955eb20b7db9b4e0ecb485e4f3bc5cab
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-06-03 16:53:40 +0000

    Deploying to gh-pages from @ Oleksii-Senchenko/project-6-node-npm-goIt@c0f9c862674dd4f15032850b5b8853df5d1e61a4 🚀

---
+++ index.93a1ae4e.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector("ul");
+//# sourceMappingURL=index.93a1ae4e.js.map
---
+++ index.be4e88ac.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector("ul");
+//# sourceMappingURL=index.be4e88ac.js.map
